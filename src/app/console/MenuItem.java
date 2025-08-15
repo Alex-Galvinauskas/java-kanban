4fb0492a382a54107adb38de37d38964fe217d43
Alex-Galvinauskas <agalvinauskas@yandex.ru>
2025-08-15 19:45:08 +0300

    refactor: сделать класс MenuItem record-class

--- src/app/console/MenuItem.java
+++ src/app/console/MenuItem.java
@@ -1,19 +1,4 @@
 package app.console;
 
-public class MenuItem {
-    private final String title;
-    private final Runnable action;
-
-    public MenuItem(String title, Runnable action) {
-        this.title = title;
-        this.action = action;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public Runnable getAction() {
-        return action;
-    }
+public record MenuItem(String title, Runnable action) {
 }
