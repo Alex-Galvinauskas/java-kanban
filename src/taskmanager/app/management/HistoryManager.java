0881388c013ece87f263ae58b9329f99acf9f6f6
Alex-Galvinauskas <agalvinauskas@yandex.ru>
2025-08-30 08:42:50 +0300

    refactor: вернуть начальное расположение интерфейсов

---
+++ src/taskmanager/app/management/HistoryManager.java
@@ -0,0 +1,16 @@
+package taskmanager.app.management;
+
+import taskmanager.app.entity.Task;
+
+import java.util.List;
+
+public interface HistoryManager {
+
+    List<Task> getHistory();
+
+    void add(Task task);
+
+    void remove(int id);
+
+    void clear();
+}
